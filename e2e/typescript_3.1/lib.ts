c93babcd7a6520adea49ccee43c72b82aefee2ee
globegitter <markus.padourek@ecosia.org>
2019-03-21 22:13:10 +0100

    Fix e2e test.
    Format.
    
    Bundle with dep is working.
    
    Fix package json.
    
    Fix lint.
    
    Fix lint 2.
    
    Remove app library

---
+++ e2e/typescript_3.1/lib.ts
@@ -0,0 +1,10 @@
+import {format} from 'date-fns'
+
+<<<<<<< HEAD
+export function sayDate() {
+  return 'hello ' + format(new Date(2014, 1, 11), 'MM/dd/YYYY');
+=======
+export function sayFive() {
+  return 'Hello ' + format(new Date(2014, 1, 11), 'MM/dd/YYYY');
+>>>>>>> 5e66400... Format.
+}
--- e2e/typescript_3.1/main.ts
+++ e2e/typescript_3.1/main.ts
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 export function test() {
   return 'test';
+=======
+import {sayDate} from './lib';
+
+console.log(sayDate());
+
+export function test() {
+  return `test ${sayDate()}`;
+>>>>>>> 93ad8f5... Fix e2e test.
+=======
+import {sayFive} from './lib';
+
+console.log(sayFive());
+
+export function test() {
+  return `test ${sayFive()}`;
+>>>>>>> 5e66400... Format.
 }
