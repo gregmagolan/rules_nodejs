a3aa34c56753315005d158ccf2de6a83441fdf5b
Greg Magolan <gmagolan@gmail.com>
2019-04-20 12:18:42 -0700

    Add a clean script for testing

---
+++ scripts/clean_all.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+# -e: exits if a command fails
+# -u: errors if an variable is referenced before being set
+# -o pipefail: causes a pipeline to produce a failure return code if any command errors
+
+readonly RULES_NODEJS_DIR=$(cd $(dirname "$0")/..; pwd)
+cd ${RULES_NODEJS_DIR}
+
+echo_and_run() { echo "+ $@" ; "$@" ; }
+
+echo_and_run rm -rf ./dist
+echo_and_run rm -rf ./node_modules
+echo_and_run rm -rf ./examples/program/node_modules
+echo_and_run rm -rf ./internal/history-server/node_modules
+echo_and_run rm -rf ./internal/http-server/node_modules
+echo_and_run rm -rf ./internal/npm_install/test/package/node_modules
+echo_and_run rm -rf ./internal/npm_install/node_modules
+echo_and_run rm -rf ./internal/rollup/node_modules
+echo_and_run rm -rf ./internal/web_package/node_modules
+echo_and_run rm -rf ./internal/e2e/fine_grained_deps/npm/node_modules
+echo_and_run rm -rf ./internal/e2e/fine_grained_deps/yarn/node_modules
+echo_and_run rm -rf ./internal/e2e/fine_grained_no_bin/node_modules
+
+echo_and_run bazel clean --expunge
+
+(
+  cd internal/e2e
+  for subDir in $(ls) ; do
+    [[ -d "${subDir}" ]] || continue
+    (
+      cd ${subDir}
+      if [[ -e 'WORKSPACE' ]] ; then
+        printf "\n\nCleaning /internal/e2e/${subDir}\n"
+        echo_and_run bazel clean --expunge
+        echo_and_run rm -rf node_modules
+      fi
+    )
+  done
+)
