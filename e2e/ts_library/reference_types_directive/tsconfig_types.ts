6965c8bdd61dda06c53adab5ff0ef4e52dc90d72
Greg Magolan <gmagolan@gmail.com>
2019-06-17 10:20:13 -0700

    fix: cleanup e2e/ts_library/reference_types_directive:tsconfig_types

--- e2e/ts_library/reference_types_directive/tsconfig_types.ts
+++ e2e/ts_library/reference_types_directive/tsconfig_types.ts
@@ -1,4 +1,6 @@
-describe('@jasmine/types types are included via tsconfig.json "types"=["jasmine"]', () => {});
+// Cecking that `describe` is a valid symbol here to ensure
+// that @types/jasmine is getting picked up
+describe('', () => {});
 // Since "hammerjs" is not included in the types=[] array in
 // tsconfig, this should result in a compile error: TS2304: Cannot find name 'Hammer'
 console.log(typeof Hammer);
